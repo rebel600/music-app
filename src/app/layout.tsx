0538ae11f745de90815564f9f2c0a1172cd1bf27
rebel600 <shyampareek232@gmail.com>
2024-03-13 00:40:43 +0530

    accertinity navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+  return ( 
+    <html lang="en" className="dark">
+      <body className="bg-black">
+        <div className="relative flex justify-center items-center w-full">
+        <Navbar />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
